7c01f356ade86baa6638d72611da87201429be0a
mekonnin <mekonninmariam12@gmail.com>
2022-03-15 19:40:12 -0400

    views.py

--- posts/views.py
+++ posts/views.py
@@ -22,7 +22,7 @@ def index(request):
             #No, Show Error
             return HttpResponseRedirect(form.errors.as_json())
     #Get all posts, limit = 20
-    posts = Post.objects.all().order_by('created_at')[:20]
+    posts = Post.objects.all().order_by('-created_at')[:20]
          
     #Show
     return render(request,'posts.html',
